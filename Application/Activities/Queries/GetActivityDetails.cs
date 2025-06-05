38b7662f7a619796ef90b8e1ffaba0393f04fab3
mohammed.Zr <mohamed.zrirake@gmail.com>
2025-06-05 21:47:25 +0100

    CRUD operations on the Frontend

---
+++ Application/Activities/Queries/GetActivityDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities.Queries
+{
+    public class GetActivityDetails
+    {
+        public class Query : IRequest<Activity>
+        {
+            public string Id { get; set; }
+        }
+        public class Handler(AppDbContext context) : IRequestHandler<Query, Activity>
+        {
+            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activity = await context.Activities.FindAsync([request.Id], cancellationToken);
+                if (activity == null) throw new Exception("Activity not found");
+                return activity;
+            }
+        }
+    }
+}
