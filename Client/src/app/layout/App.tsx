cbd24604b7ef85789ab15bad8215061e3bdadadd
mohammed.Zr <mohamed.zrirake@gmail.com>
2025-06-14 00:54:10 +0100

    Adding some styles using Material UI components

--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -1,17 +1,23 @@
 
 import NavBar from "./NavBar";
 import { Container, CssBaseline } from "@mui/material";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/home/HomePage";
 
 
    function App(){
+      const location = useLocation();
    return (
    <div>
       <CssBaseline/>
-      <NavBar/>
+      {location.pathname === '/' ?<HomePage/> :(
+         <>
+          <NavBar/>
       <Container maxWidth='xl' sx={{mt:3}} >
        <Outlet/>
       </Container>
+         </>
+      )}
   </div>
    )
    }
--- Client/src/features/home/HomePage.tsx
+++ Client/src/features/home/HomePage.tsx
@@ -1,10 +1,45 @@
-import {Container, Typography } from "@mui/material"
+import { Group } from "@mui/icons-material"
+import {Box, Button, Link, Paper, Typography } from "@mui/material"
 
 
 export default function HomePage(){
     return (
-        <Container sx={{mx:3}}>
-            <Typography variant="h3">Home Page</Typography>
-        </Container>
+        <Paper sx={{
+            color:'white',
+            display:'flex',
+            flexDirection:'column',
+            alignItems:'center',
+            gap:6,
+            alignContent:'center',
+            justifyContent:'center',
+            height:'100vh',
+            backgroundImage:'linear-gradient(135deg, #182a73  0% , #218aae 69%, #20a7ac 89%)'
+        }}>
+
+<Box sx={{
+    display:'flex',
+    alignContent:'center',
+    alignItems:'center',
+    color:'white',
+    gap:3
+}}>
+    <Group sx={{height:10, width:110 }}/>
+    <Typography variant="h1">
+Reactivities
+    </Typography>
+</Box>
+<Typography variant="h2">
+    Welcome to reactivities
+</Typography>
+<Button
+component={Link}
+href='/activities'
+size="large"
+variant="contained"
+sx={{height:80, borderRadius:4, fontSize:'1.5rem'}}
+>
+    Take me to the activities!
+</Button>
+        </Paper>
     )
 }
