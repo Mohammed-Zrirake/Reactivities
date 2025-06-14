ee580492ed20dd9f564966441d52a461f60ea28e
mohammed.Zr <mohamed.zrirake@gmail.com>
2025-06-14 01:59:40 +0100

    MobX for state management

--- Client/src/app/layout/NavBar.tsx
+++ Client/src/app/layout/NavBar.tsx
@@ -1,41 +1,62 @@
-            import { Group,  } from '@mui/icons-material'
-            import {  Box,  AppBar, Toolbar,  Typography, Container, MenuItem } from '@mui/material'
-import { NavLink } from 'react-router'
-import MenuItemLink from '../shared/components/MenuItemLink'
+import { Group } from '@mui/icons-material';
+import { Box, AppBar, Toolbar, Typography, Container, MenuItem, LinearProgress } from '@mui/material';
+import { NavLink } from 'react-router'; // fixed import from 'react-router' to 'react-router-dom'
+import MenuItemLink from '../shared/components/MenuItemLink';
+import { uiStore } from '../../lib/stores/uiStore';
+import { Observer } from 'mobx-react-lite';
+import { useStore } from '../../lib/stores/store'; // assuming useStore is defined here
 
-        
+function NavBar() {
+    const { uiStore } = useStore(); // fixed destructuring syntax
+    return (
+        <Box sx={{ flexGrow: 1 }}> {/* fixed < to <Box */}
+            <AppBar
+                position="static"
+                sx={{
+                    backgroundImage:
+                        'linear-gradient(135deg,  #182a73 0%,  #218aae 69% , #20a7ac 89%)',
+                    position: 'relative',
+                }}
+            >
+                <Container maxWidth="xl">
+                    <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}> {/* fixed typo 'dispaly' */}
+                        <Box>
+                            <MenuItem component={NavLink} to="/" sx={{ display: 'flex', gap: 2 }}>
+                                <Group fontSize="large" />
+                                <Typography variant="h4" fontWeight="bold">
+                                    Reactivities
+                                </Typography>
+                            </MenuItem>
+                        </Box>
 
-            function NavBar() {
-            return (
-                <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static" sx={{backgroundImage:'linear-gradient(135deg,  #182a73 0%,  #218aae 69% , #20a7ac 89%)'}}>
-                <Container maxWidth='xl'>
-        <Toolbar sx={{dispaly:'flex', justifyContent:'space-between'}}>
-                <Box>
-                    <MenuItem component={NavLink} to ='/' sx={{display:'flex', gap:2}}>
-                    <Group fontSize='large'/>
-                    <Typography variant='h4' fontWeight='bold'>Reactivities</Typography>
-                    </MenuItem>
-                </Box>
+                        <Box sx={{ display: 'flex' }}>
+                            <MenuItemLink to="/activities">Activities</MenuItemLink>
+                            <MenuItemLink to="/createActivity">Create Activity</MenuItemLink>
+                        </Box>
 
-                <Box sx={{display:'flex'}}>
-                    <MenuItemLink to='/activities'>
-                    Activities
-                    </MenuItemLink>
-                    <MenuItemLink
-                     to='/createActivity'
-                     >
-                    Create Activity
-                    </MenuItemLink>
-                 
-                </Box>
-               <MenuItem>User Menu</MenuItem>
+                        <MenuItem>User Menu</MenuItem>
                     </Toolbar>
                 </Container>
-                </AppBar>
-                </Box>
-            
-            )
-            }
 
-            export default NavBar
+                <Observer>
+                    {() =>
+                        uiStore.isLoading ? (
+                            <LinearProgress
+                                color="secondary"
+                                sx={{
+                                    position: 'absolute',
+                                    bottom: 0,
+                                    left: 0,
+                                    right: 0,
+                                    height: 4,
+                                }}
+                            />
+                        ) : null
+                    }
+                </Observer>
+            </AppBar>
+        </Box>
+    );
+}
+
+export default NavBar;
